3007b597f234c0445b246295d43717359594d2bb
waleron07 <sbubin1990@gmail.com>
2020-06-16 17:24:04 +0300

    Updates

---
+++ precache-manifest.fc1eee8c2c5509fc03cb3810900e9751.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "571535efeb75b615aea42ec5fbe0b16a",
+    "url": "/blogging-platform/index.html"
+  },
+  {
+    "revision": "ed7b0014d3c1ddd3b386",
+    "url": "/blogging-platform/static/css/2.b226b7fb.chunk.css"
+  },
+  {
+    "revision": "074a7253ad67fff7ccd9",
+    "url": "/blogging-platform/static/css/main.08aed40b.chunk.css"
+  },
+  {
+    "revision": "ed7b0014d3c1ddd3b386",
+    "url": "/blogging-platform/static/js/2.f56b8f19.chunk.js"
+  },
+  {
+    "revision": "885c0e205ef5e79f4aef448a818ba6a1",
+    "url": "/blogging-platform/static/js/2.f56b8f19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "074a7253ad67fff7ccd9",
+    "url": "/blogging-platform/static/js/main.78a28e9e.chunk.js"
+  },
+  {
+    "revision": "17f06f8e5ba79a415e84",
+    "url": "/blogging-platform/static/js/runtime-main.12d03998.js"
+  }
+]);
